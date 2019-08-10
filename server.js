c78537d81ebb131c9a1075960f6e3e8f64770745
uptheirons78 <bono.mauro@gmail.com>
2019-08-10 15:10:32 +0200

    server setup with routes

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.json({msg: 'Welcome to the Contact-Keeper API'}));
+
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
